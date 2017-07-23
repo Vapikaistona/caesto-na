b26a54cb8b9173b5d2272b142fa66baa40c614c4
Eduardo <eroblesortiz@gmail.com>
2017-07-23 20:06:05 +0200

    Componentes usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,48 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
 import { UsuarioComponent } from './usuario/usuario.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+
+const appRoutes: Routes = [
+  { path: 'users/:id',      component: UserDetailComponent },
+  {
+    path: 'users',
+    component: UserListComponent,
+    data: { title: 'Users List' }
+  },
+  { path: '',
+    redirectTo: '/users',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsuarioComponent
+    UsuarioComponent,
+    PageNotFoundComponent,
+    UserListComponent,
+    UserDetailComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+    // other imports here
+  ]
+  ,
   providers: [],
   bootstrap: [AppComponent]
 })
