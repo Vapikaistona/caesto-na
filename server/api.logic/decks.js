11d5171ca45c36fbdf71aa1c4eeb6d0eb9a5dad7
Eduardo Robles <erobles@externos.bankia.es>
2017-09-11 16:49:50 +0200

    Server cache decks

--- server/api.logic/decks.js
+++ server/api.logic/decks.js
@@ -3,20 +3,25 @@ var Deck = require('../models/deck');
 var Race = require('../models/race');
 var User = require('../models/user');
 var dateFormat = require('dateformat');
-
+var deckCache =[];
 var decks={};
 
 
 decks.getAllDecks =  (req, res) => {
+  if (deckCache.length ==0) {
     Deck.find({},(err,decklist)=>{
       if (err){
         console.log("error "+ err);
         res.send(JSON.err)
       }
       else {
+        deckCache = decklist;
         res.send(decklist);
       }
     });
+  }else {
+    res.send(deckCache);
+  }
 };
 decks.getOwnedDecks =  (req, res) => {
     User.findById(req.params.id,(err,user)=>{
@@ -67,12 +72,13 @@ decks.insertDeck =  (req, res) => {
           if (req.decoded._doc.username == deckF.created_by || req.decoded._doc.lvl == 3) {
             deck.modified_by =req.decoded._doc.username;
             deck.modified =  dateFormat(now,"dd/mm/yyyy, HH:MM:ss");
-            Deck.findOneAndUpdate({_id:deck.id},deck,(err,deck)=>{
+            Deck.findOneAndUpdate({_id:deck.id},deck,{new:true},(err,deckUpdated)=>{
               if (err){
                 console.log("error "+ err);
                 res.send(JSON.err)
               }
               else {
+                deckCache[deckCache.findIndex(x=>x._id+"" === deckUpdated._id+"")] =deckUpdated;
                 res.send({ok:true});
               }
             });
@@ -87,15 +93,8 @@ decks.insertDeck =  (req, res) => {
               res.send(JSON.err);
             }
             else {
-              User.findById(req.decoded._doc._id,(err,user)=>{
-                if (err){
-                  console.log("error "+ err);
-                  res.send(JSON.err);
-                }
-                else {
-                  res.send({ok:true});
-                }
-              })
+              deckCache.push(deck);
+              res.send({ok:true});
             }
           });
         }
@@ -123,6 +122,7 @@ decks.deleteDeck = (req, res) => {
       res.send(JSON.err)
     }
     else {
+      deckCache.splice(deckCache.findIndex(x=>x._id == req.params.id),1);
       res.send(deck);
     }
   });
